251cb6e076b033dabcabf225488d4b79671d54b0
Riya Bisht <hack1001infinity@gmail.com>
2024-10-14 18:31:17 +0530

    add files

---
+++ outflow/LED_BLINKING_DEMO.interface.io
@@ -0,0 +1,16 @@
+# Efinity IO Placement 
+# Interface File: /home/trinity/Downloads/efinity/2024.1/project/LED_BLINKING_DEMO/outflow/LED_BLINKING_DEMO.interface.csv
+# Version:        2024.1.163
+# Date:           Jun 25 2024
+
+# Copyright (C) 2013 - 2024  All rights reserved.
+
+
+# pin name	x	y	z
+# --------	----	----	---
+led_3     	307	643	0
+led_4     	308	643	0
+led_2     	322	643	0
+led_1     	323	643	0
+i_clk     	338	5	1
+clk_in    	338	323	1
---
+++ outflow/LED_BLINKING_DEMO_template.v
@@ -0,0 +1,32 @@
+
+// Efinity Top-level template
+// Version: 2024.1.163
+// Date: 2024-10-14 18:22
+
+// Copyright (C) 2013 - 2024 Efinix Inc. All rights reserved.
+
+// This file may be used as a starting point for Efinity synthesis top-level target.
+// The port list here matches what is expected by Efinity constraint files generated
+// by the Efinity Interface Designer.
+
+// To use this:
+//     #1)  Save this file with a different name to a different directory, where source files are kept.
+//              Example: you may wish to save as /home/trinity/Downloads/efinity/2024.1/project/LED_BLINKING_DEMO/LED_BLINKING_DEMO.v
+//     #2)  Add the newly saved file into Efinity project as design file
+//     #3)  Edit the top level entity in Efinity project to:  LED_BLINKING_DEMO
+//     #4)  Insert design content.
+
+
+module LED_BLINKING_DEMO
+(
+  (* syn_peri_port = 0 *) input i_clk,
+  (* syn_peri_port = 0 *) input clk_in,
+  (* syn_peri_port = 0 *) output led_1,
+  (* syn_peri_port = 0 *) output led_2,
+  (* syn_peri_port = 0 *) output led_3,
+  (* syn_peri_port = 0 *) output led_4
+);
+
+
+endmodule
+
